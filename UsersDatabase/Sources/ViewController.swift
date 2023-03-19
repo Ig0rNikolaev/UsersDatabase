28e77448194a6b8467021cebe106eea3f75a9283
Igor Nikolaev <igor01131987@gmail.com>
2023-03-19 20:11:47 +0300

    Add setup project

---
+++ UsersDatabase/Sources/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  UsersDatabase
+//
+//  Created by Игорь Николаев on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //: MARK: - UI Elements
+
+
+
+
+    //: MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    //: MARK: - Setups
+
+    private func setupView() {
+
+    }
+
+    private func setupHierarchy() {
+
+
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+
+        ])
+    }
+}

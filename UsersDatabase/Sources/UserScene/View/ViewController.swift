5e2c038c9c19076d3baeaa4418bca305228bc592
Igor Nikolaev <igor01131987@gmail.com>
2023-03-20 13:56:17 +0300

    Add first scene

---
+++ UsersDatabase/Sources/UserScene/View/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  UsersDatabase
+//
+//  Created by Игорь Николаев on 19.03.2023.
+//
+
+import UIKit
+
+class UserView: UIViewController {
+
+    //: MARK: - UI Elements
+
+    private lazy var userNameText: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = .systemGray6
+        textField.textAlignment = .center
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder ?? "Print your name")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private lazy var userButtonPress: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add user", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(addUser), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var userTable: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    //: MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    //: MARK: - Setups
+
+    @objc func addUser() {}
+
+    private func setupView() {
+        title = "USERS"
+        view.backgroundColor = .white
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(userNameText)
+        view.addSubview(userButtonPress)
+        view.addSubview(userTable)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            userNameText.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            userNameText.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            userNameText.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            userNameText.bottomAnchor.constraint(equalTo: userNameText.topAnchor, constant: 45),
+
+            userButtonPress.topAnchor.constraint(equalTo: userNameText.bottomAnchor, constant: 10),
+            userButtonPress.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            userButtonPress.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            userButtonPress.bottomAnchor.constraint(equalTo: userButtonPress.topAnchor, constant: 45),
+
+            userTable.topAnchor.constraint(equalTo: userButtonPress.bottomAnchor, constant: 10),
+            userTable.leftAnchor.constraint(equalTo: view.leftAnchor),
+            userTable.rightAnchor.constraint(equalTo: view.rightAnchor),
+            userTable.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension UserView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "User name"
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+}
+
+extension UserView: UITableViewDelegate {
+
+}
